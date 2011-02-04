a9eb2a9528544c58680ab57cda3b4f5bacf9a84d
selead <allselead@gmail.com>
2011-02-05 01:29:20 +0300

    Refactor filenames and export data.

---
+++ main.js
@@ -0,0 +1,27 @@
+//module.exports = require('./lib');
+
+exports.version = '0.1.4';
+
+exports.server = require('./lib/server');
+exports.util = require('./lib/util');
+exports.Consumer = require('./lib/data').Consumer;
+
+var fs = require('fs');
+
+exports.store = {};
+
+fs.readdirSync(__dirname + '/lib/store').forEach(function(filename){
+    if (/\.js$/.test(filename)) {
+        var name = filename.substr(0, filename.lastIndexOf('.'));
+        Object.defineProperty(exports.store, name, { get: function(){
+            return require('./lib/store/' + name);
+        }});
+    }
+});
+
+
+/**
+* Expose getters as first-class exports.
+*/
+
+exports.__proto__ = exports.store;
